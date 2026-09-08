c70cc92b78399781e0b14b6e09502727d15ac829
Yosef <mercytv932@gmail.com>
2026-09-08 11:39:31 -0400

    sdd/minus function done

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -3,11 +3,27 @@ import { useEffect } from "react";
 
 function AdvancedCounter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleAdd() {
+    setCount(count + 1);
+  }
+
+  function handleMinus() {
+    setCount(count - 1);
+  }
   return (
     <div>
       <h3>
         Counter: <span>{count}</span>
       </h3>
+
+      <div>
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleMinus}>Minus</button>
+      </div>
+
+      <input type="number" placeholder="choose a number..." />
     </div>
   );
 }
